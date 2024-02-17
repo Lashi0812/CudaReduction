abad11b51308b6502145b2969d9159fed9a62c58
Lashi <lashi0812@gmail.com>
2024-02-17 06:47:58 +0000

    single kernel iterative reduction

---
+++ README.md
@@ -0,0 +1,18 @@
+# Iterative Parallel Reduction Algorithms
+
+## Introduction
+
+In parallel computing, reduction operations involve combining elements of a dataset to produce a single result. Iterative parallel reduction algorithms are commonly used to efficiently perform reductions on large datasets in parallel environments, such as GPUs. This documentation outlines two iterative parallel reduction algorithms: the first algorithm utilizes multiple kernels to reduce the dataset iteratively, while the second algorithm employs a single kernel with multiple stages of reduction.
+
+
+> [!NOTE]  
+> Use excalidraw extension in vscode for view the description for each algo in the [docs](docs/Reduction.excalidraw)
+> or use [online version](https://excalidraw.com/#json=dLMM6ZB8zCMZT8ZKQStc7,HI273yQF3QzvGIIqHmDXKw)
+
+
+## Algorithm 1: Multi-Kernel Iterative Reduction
+Suppose we have the 2^N elements we launch N kernels each time we reduce the half number total number threads.Here we have 2^4 = 16 elements so we launch the 4 kernel and each time we launch half the threads
+![alt text](assets/reduce0.png)
+
+## Algorithm 2: Single-Kernel Iterative Reduction
+![alt text](assets/reduce1.png)
---
+++ src/reduce1.cu.cc
@@ -0,0 +1,70 @@
+#include "cuda/include/thrust/device_vector.h"
+#include "cuda/include/thrust/host_vector.h"
+#include "include/nvToolsExt.h"
+#include <iostream>
+#include <random>
+
+__global__ void reduce1(float *x, int m) {
+    int   tid         = blockDim.x * blockIdx.x + threadIdx.x;
+    int   stride      = gridDim.x * blockDim.x;
+    float partial_sum = 0.0f;
+    for (int k{tid}; k < m; k += stride) {
+        partial_sum += x[k];
+    }
+    x[tid] = partial_sum;
+}
+
+template <typename T>
+void init_data(thrust::host_vector<T> &x) {
+    std::default_random_engine        seed(12345678);
+    std::uniform_real_distribution<T> dist(0.0, 1.0);
+    for (auto &elem : x)
+        elem = dist(seed);
+}
+
+template <typename T>
+double host_reduce1(const thrust::host_vector<T> &x) {
+    RANGE("host call");
+    double result = 0.0;
+    for (const auto &elem : x) {
+        result += elem;
+    }
+    return result;
+}
+
+template <typename T>
+void device_reduce1(
+  thrust::device_vector<T> &d_x, const int N, const int blocks, const int threads) {
+    RANGE("device call");
+    reduce1<<<blocks, threads>>>(d_x.data().get(), N);
+    reduce1<<<1, threads>>>(d_x.data().get(), blocks * threads);
+    reduce1<<<1, 1>>>(d_x.data().get(), threads);
+    cudaDeviceSynchronize();
+}
+
+int main(int argc, char *argv[]) {
+    int N       = (argc > 1) ? atoi(argv[1]) : 1 << 24; // default 2**24
+    int blocks  = (argc > 2) ? atoi(argv[2]) : 288;
+    int threads = (argc > 3) ? atoi(argv[3]) : 256;
+
+    thrust::host_vector<float>   x(N);
+    thrust::device_vector<float> dev_x(N);
+
+    // initialise x with random numbers and copy to dx
+    init_data(x);
+
+    dev_x          = x; // H2D copy (N words)
+    float host_sum = host_reduce1(x);
+
+    device_reduce1(dev_x, N, blocks, threads);
+
+    double gpu_sum = dev_x[0]; // D2H copy (1 word)
+    // clang-format off
+    std::cout << "Sum of " << N << " random Numbers in reduce1" 
+              << "\n\thost : " << std::fixed << host_sum 
+              << "\n\tGPU  : " << std::fixed << gpu_sum 
+              << "\nand " << (host_sum == gpu_sum ? "✅ Sum Match " : "❌ Not Match ") 
+              << std::endl;
+    // clang-format on
+    return 0;
+}
